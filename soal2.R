015ce24bf97bcf00da1c34bd9304dba10ab8af24
Maula Izza Azizi <72302421+storyofhis@users.noreply.github.com>
2022-05-29 21:01:59 +0700

    soal2

---
+++ soal2.R
@@ -0,0 +1,17 @@
+# soal 2
+install.packages("BSDA")
+library(BSDA)
+
+# 2a
+# Setuju
+
+# 2b
+tsum.test(mean.x=23500, sd(3900), n.x=100)
+
+# 2c
+# Buatlah kesimpulan berdasarkan P-Value yang dihasilkan!
+# Untuk mencari nilai z nya yaitu
+# image-removebg-preview
+# Lalu mencari nilai p-value nya sebagai berikutimage-removebg-preview (1)image-removebg-preview (2)
+# Sehingga kesimpulan yang didapat adalah bahwa mobil dikemudikan rata-rata lebih dari 20.000 kilometer per tahun
+
